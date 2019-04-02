225a0122327fc3143f46c2c4d7e8ca05d1a6e7e5
Dylan Hofmann <dylan.hofmann@gmail.com>
2019-04-01 20:22:40 -0600

    Incorporate Redux into index.js

--- src/index.js
+++ src/index.js
@@ -3,8 +3,22 @@ import ReactDOM from 'react-dom';
 import './styles/main.scss';
 import App from './containers/App/App';
 import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, createStore } from 'redux';
+import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { rootReducer } from './reducers';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const devTools = composeWithDevTools(applyMiddleware(thunk));
+
+const store = createStore(rootReducer, devTools)
+
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
