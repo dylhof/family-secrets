d81998bdfc346ea2db5c6bfe7090832526acb12e
Dylan Hofmann <dylan.hofmann@gmail.com>
2019-04-04 16:24:50 -0600

    Add currentView from redux to CardArea

--- src/containers/CardArea/CardArea.js
+++ src/containers/CardArea/CardArea.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
-export const CardArea = () => {
+export const CardArea = (props) => {
+  const { currentView } = props;
   return (
     <div className='CardArea--div'>
-      <h2>My Family Stories</h2>
+      <h2>My Family {currentView}</h2>
     </div>
   )
 }
 
-export default CardArea;
+export const mapStateToProps = state => ({
+  currentView: state.currentView
+})
+export default connect(mapStateToProps)(CardArea);
+
+CardArea.propTypes = {
+  currentView: PropTypes.string
+}
