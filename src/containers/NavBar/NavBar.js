cbf37c978d4f391265d1e1df6ca9c8c47ce96628
Dylan Hofmann <dylan.hofmann@gmail.com>
2019-04-04 13:49:42 -0600

    Add option to NavBar

--- src/containers/NavBar/NavBar.js
+++ src/containers/NavBar/NavBar.js
@@ -6,6 +6,7 @@ export class NavBar extends Component{
       <nav className='NavBar--nav'>
         <select>
           <option>Select a Family</option>
+          <option>Family1</option>
         </select>
         <ul>
           <li>Stories</li>
