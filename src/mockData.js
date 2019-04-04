792fa9783c50cb43dc719f9ce60d19569e5a683c
Dylan Hofmann <dylan.hofmann@gmail.com>
2019-04-04 16:23:42 -0600

    Add mockData file

---
+++ src/mockData.js
@@ -0,0 +1,19 @@
+export const mockUser = {
+  id: 1,
+  name: 'Betty',
+}
+
+
+export const families = [
+  {
+    name: 'Reitmeyer',
+    id: 2,
+    user_id: 1
+  },
+  {
+    name: 'Hofmann',
+    id: 3,
+    user_id: 1
+  }
+]
+
