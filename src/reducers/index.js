5e9a763495294f5033beccb248cbda4f175d685f
Dylan Hofmann <dylan.hofmann@gmail.com>
2019-04-04 16:27:00 -0600

    Add currentView and currentFamily to redux Store

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,13 @@
 import { combineReducers } from 'redux';
 import { errorReducer } from './errorReducer';
 import { loadingReducer } from './loadingReducer';
+import { currentViewReducer } from './currentViewReducer';
+import { currentFamilyReducer } from './currentFamilyReducer';
 
 
 export const rootReducer = combineReducers({
   error: errorReducer,
-  isLoading: loadingReducer
+  isLoading: loadingReducer,
+  currentView: currentViewReducer,
+  currentFamily: currentFamilyReducer
 });
