6ea69b75f13e16d5f065b52ff80c5a47d5e62b64
Dylan Hofmann <dylan.hofmann@gmail.com>
2019-04-06 16:37:02 -0600

    Add functionality to fetch stories, images and recipes from nav bar

--- src/containers/NavBar/NavBar.js
+++ src/containers/NavBar/NavBar.js
@@ -1,15 +1,32 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { setCurrentView, setCurrentFamily } from '../../actions';
-import * as mockData from '../../mockData';
+import { apiThunk } from '../../thunks/apiThunk';
+// import * as mockData from '../../mockData';
 
 export class NavBar extends Component{
   setView = (event) => {
-    this.props.setCurrentView(event.target.innerText);
+    const {currentFamily} = this.props
+    let view = event.target.innerText.toLowerCase();
+
+    if(view === 'photos') {
+      view = 'images'
+    };
+
+    const content = this.props[view].findIndex(item => {
+      return item.family_id === currentFamily
+    })
+
+    if( content && currentFamily) {
+      this.props.apiThunk(`families/${currentFamily}/${view}`, `set${event.target.innerText}`)
+    }
+
+    this.props.setCurrentView(view);
   }
   
   generateOptions = () => {
-    const options = mockData.families.map(family => {
+    const { families } = this.props   
+    const options = families.map(family => {
       return (
         <option key={family.id} value={family.id}>{family.name}</option>
       )
@@ -41,7 +58,16 @@ export class NavBar extends Component{
 
 export const mapDispatchToProps = dispatch => ({
   setCurrentView: (currentView) => dispatch(setCurrentView(currentView)),
-  setCurrentFamily: (id) => dispatch(setCurrentFamily(id))
+  setCurrentFamily: (id) => dispatch(setCurrentFamily(id)),
+  apiThunk: (path, action) => dispatch(apiThunk(path, action))
 });
 
-export default connect(null, mapDispatchToProps)(NavBar);
+export const mapStateToProps = state => ({
+  families: state.families,
+  currentFamily: state.currentFamily,
+  stories: state.stories,
+  recipes: state.recipes,
+  images: state.photos
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(NavBar);
