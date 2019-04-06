2b6e65dea11081a042ac2a228a9b977843dd7b64
Dylan Hofmann <dylan.hofmann@gmail.com>
2019-04-06 11:47:45 -0600

    Add currentUser to state tree

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,11 +3,13 @@ import { errorReducer } from './errorReducer';
 import { loadingReducer } from './loadingReducer';
 import { currentViewReducer } from './currentViewReducer';
 import { currentFamilyReducer } from './currentFamilyReducer';
+import { currentUserReducer } from './currentUserReducer';
 
 
 export const rootReducer = combineReducers({
   error: errorReducer,
   isLoading: loadingReducer,
   currentView: currentViewReducer,
-  currentFamily: currentFamilyReducer
+  currentFamily: currentFamilyReducer,
+  currentUser: currentUserReducer
 });
