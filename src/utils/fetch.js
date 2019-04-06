43494dd216250d9308cfdb75b52e652921f67499
Dylan Hofmann <dylan.hofmann@gmail.com>
2019-04-06 16:37:49 -0600

    Add environmental variable for fetch call

--- src/utils/fetch.js
+++ src/utils/fetch.js
@@ -1,11 +1,12 @@
 export const fetchData = async (path, options) => {
   // may need to account for different responses in my conditional
-  const response = await fetch(`https://sheltered-badlands-12986.herokuapp.com/api/v1/${path}`, options)
+  const response = await fetch(`${process.env.REACT_APP_BACKEND_URL}api/v1/${path}`, options)
   if (response.status >= 300) {
     const error = await response.json()
     throw new Error(error)
   } else { 
-    return response.json
+    const data = await response.json()
+    return data
   }
 };
 
@@ -16,4 +17,3 @@ export const createOptions = (method, data) => ({
     'Content-Type': 'application/json'
   }
 });
-
