ef9d0840bdcc14043bc54ed6adfd3e1ff00a829e
Dylan Hofmann <dylan.hofmann@gmail.com>
2019-04-07 11:51:36 -0600

    Add ADD_FAMILY, DELETE_FAMILY and UPDATE_FAMILY types to families Reducer and tests for those types

--- src/reducers/__tests__/familiesReducer.js
+++ src/reducers/__tests__/familiesReducer.js
@@ -9,9 +9,38 @@ describe('familiesReducer', () => {
     expect(result).toEqual(expected);
   });
 
-  it('should return an id when type is SET_FAMILIES', () => {
+  it('should return families when type is SET_FAMILIES', () => {
     const initialState = []
-    const result = familiesReducer(initialState, actions.setFamilies(mockData.families));
-    expect(result).toEqual(mockData.families);
+    const mockFamilies = mockData.families
+    const result = familiesReducer(initialState, actions.setFamilies(mockFamilies));
+    expect(result).toEqual(mockFamilies);
+  });
+
+  it('should return families with an the additional family', () => {
+    const initialState = mockData.families;
+    const mockFamily = {
+        name: 'Smith',
+        id: 4,
+        user_id: 1
+    };
+    const expected = [...initialState, mockFamily];
+    const result = familiesReducer(initialState, actions.addFamily(mockFamily));
+    expect(result).toEqual(expected)
+  });
+
+  it('should return fewer families when type is DELETE_FAMILY', () => {
+    const initialState = mockData.families;
+    const mockId = mockData.families[0].id;
+    const expected = [mockData.families[1]];
+    const result = familiesReducer(initialState, actions.deleteFamily(mockId));
+    expect(result).toEqual(expected)
+  });
+
+  it('shoud return families with an updated family', () => {
+    const initialState = mockData.families;
+    const mockUpdatedFamily = {...mockData.families[0], name: 'Jones'};
+    const expected = [mockUpdatedFamily, mockData.families[1]];
+    const result = familiesReducer(initialState, actions.updateFamily(mockUpdatedFamily));
+    expect(result).toEqual(expected)
   });
 });
