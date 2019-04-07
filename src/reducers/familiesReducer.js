735517868bd10461078072ea305bb29d3f3cdb0d
Dylan Hofmann <dylan.hofmann@gmail.com>
2019-04-07 13:23:17 -0600

    Change families reducer to not mutate state

--- src/reducers/familiesReducer.js
+++ src/reducers/familiesReducer.js
@@ -5,12 +5,14 @@ export const familiesReducer = (state = [], action) => {
     case 'ADD_FAMILY':
       return [...state, action.family]
     case 'DELETE_FAMILY':
-      const filteredFamilies = state.filter(family=> {
+      const familiesToFilter = [...state]
+      const filteredFamilies = familiesToFilter.filter(family=> {
         return family.id !== action.id
       })
       return filteredFamilies
     case 'UPDATE_FAMILY':
-      const updatedFamilies = state.map(family => {
+      const familiesToMap = [...state]
+      const updatedFamilies = familiesToMap.map(family => {
         if(family.id === action.family.id) {
           return action.family
         } else {
