56e19c83ed430d4df2694179c89ca0a747f456d5
Dylan Hofmann <dylan.hofmann@gmail.com>
2019-04-07 13:21:25 -0600

    Add ADD_PHOTO, DELETE_PHOTO, UPDATE_PHOTO types to photosReducer and tests

--- src/reducers/__tests__/photosReducers.js
+++ src/reducers/__tests__/photosReducers.js
@@ -14,4 +14,40 @@ describe('photosReducer', () => {
     const result = photosReducer(initialState, actions.setPhotos(mockData.photos));
     expect(result).toEqual(mockData.photos);
   });
+
+  it('should add a photo when the type is ADD_PHOTO', () => {
+    const initialState = mockData.photos
+    const mockPhoto = {
+      photo: 'photoURL',
+      caption: 'photo',
+      family_id: 2,
+      id: 10
+    }
+    const expected = [...initialState, mockPhoto]
+    const result = photosReducer(initialState, actions.addPhoto(mockPhoto));
+    expect(result).toEqual(expected);
+    expect(result.length).toBe(initialState.length + 1);
+  });
+
+  it('should remove a photo when type is DELETE_PHOTO', () => {
+    const initialState = mockData.photos;
+    const mockId = initialState[2].id;
+    const result = photosReducer(initialState, actions.deletePhoto(mockId));
+    expect(result.length).toBe(initialState.length - 1);
+  });
+
+  it('should update a photo when type is UPDATE_PHOTO', () => {
+    const initialState = mockData.photos
+    const mockPhotoToUpdate = {
+      photo: 'https://i.imgur.com/6yHmlwT.jpg',
+    caption: 'New Title',
+    family_id: 2,
+    id: 1
+    }
+    let expected = [...initialState]
+    expected.splice(0, 1, mockPhotoToUpdate);
+    const result = photosReducer(initialState, actions.updatePhoto(mockPhotoToUpdate));
+    expect(result).toEqual(expected);
+  });
+
 });
--- src/reducers/photosReducer.js
+++ src/reducers/photosReducer.js
@@ -2,6 +2,24 @@ export const photosReducer = (state = [], action) => {
   switch(action.type) {
     case 'SET_PHOTOS':
       return action.photos
+    case 'ADD_PHOTO':
+      return [...state, action.photo]
+    case 'DELETE_PHOTO':
+      const photosToFilter = [...state]
+      const filteredPhtotos = photosToFilter.filter(photo => {
+        return photo.id !== action.id
+      })
+      return filteredPhtotos
+    case 'UPDATE_PHOTO':
+      const photosToMap = [...state]
+      const updatedPhotos = photosToMap.map(photo => {
+        if(photo.id === action.photo.id) {
+          return action.photo
+        } else {
+          return photo
+        }
+      })
+      return updatedPhotos
     default: 
       return state
   };
