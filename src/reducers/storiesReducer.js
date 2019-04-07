f13ea0ae61dc77eb7098d294b08b8253f9066cfa
Dylan Hofmann <dylan.hofmann@gmail.com>
2019-04-07 13:22:46 -0600

    Add ADD_STORY, DELETE_STORY, UPDATE_STORY types to storiesReducer and tests

--- src/reducers/storiesReducer.js
+++ src/reducers/storiesReducer.js
@@ -2,6 +2,24 @@ export const storiesReducer = (state = [], action) => {
   switch(action.type) {
     case 'SET_STORIES':
       return action.stories
+    case 'ADD_STORY':
+      return [...state, action.story]
+    case 'DELETE_STORY':
+      const storiesToFilter = [...state]
+      const filteredStories = storiesToFilter.filter(story => {
+        return story.id !== action.id
+      })
+      return filteredStories
+    case 'UPDATE_STORY':
+      const storiesToMap = [...state]
+      const updatedStories = storiesToMap.map(story => {
+        if(story.id === action.story.id) {
+          return action.story
+        } else {
+          return story
+        }
+      })
+      return updatedStories
     default: 
       return state
   };
