e649ddff27c9b46f34b5726fe5866a27910149c3
Dylan Hofmann <dylan.hofmann@gmail.com>
2019-04-07 11:06:20 -0600

    Add actions to add, delete and update families, recipes, stories and photos and tests

--- src/actions/index.test.js
+++ src/actions/index.test.js
@@ -51,7 +51,7 @@ describe('actions', () => {
     expect(result).toEqual(expected);
   });
 
-  it('should return an object with a type of LOG_OUT', () => {
+  it('should return an object with LOG_OUT', () => {
     const expected = {
       type: 'LOG_OUT'
     };
@@ -59,7 +59,7 @@ describe('actions', () => {
     expect(result).toEqual(expected);
   });
 
-  it('should return an object with a type of SET_FAMILIES', () => {
+  it('should return an object with SET_FAMILIES', () => {
     const expected = {
       type: 'SET_FAMILIES',
       families: mockData.families
@@ -68,7 +68,7 @@ describe('actions', () => {
     expect(result).toEqual(expected);
   });
 
-  it('should return an object with a type of SET_RECIPES and recipes', () => {
+  it('should return an object with SET_RECIPES and recipes', () => {
      const expected = {
        type: 'SET_RECIPES',
        recipes: mockData.recipes
@@ -77,7 +77,7 @@ describe('actions', () => {
      expect(result).toEqual(expected);
   });
 
-  it('should return an object with a type of SET_STORIES and stories', () => {
+  it('should return an object with SET_STORIES and stories', () => {
     const expected = {
       type: 'SET_STORIES',
       stories: mockData.stories
@@ -86,7 +86,7 @@ describe('actions', () => {
     expect(result).toEqual(expected);
   });
 
-  it('should return an object with a type of SET_PHOTOS and photos', () => {
+  it('should return an object with SET_PHOTOS and photos', () => {
     const expected = {
       type: 'SET_PHOTOS',
       photos: mockData.photos
@@ -95,4 +95,119 @@ describe('actions', () => {
     expect(result).toEqual(expected);
   });
 
+  it('should return an object with ADD_FAMILY and a family', () => {
+    const mockFamily = mockData.families[0];
+    const expected = {
+      type: 'ADD_FAMILY',
+      family: mockFamily
+    };
+    const result = actions.addFamily(mockFamily);
+    expect(result).toEqual(expected);
+  });
+
+  it('should return an object with ADD_RECIPE and a recipe', () => {
+    const mockRecipe = mockData.recipes[0];
+    const expected = {
+      type: 'ADD_RECIPE',
+      recipe: mockRecipe
+    };
+    const result = actions.addRecipe(mockRecipe);
+    expect(result).toEqual(expected);
+  });
+
+  it('should return an object with ADD_STORY and a story', () => {
+    const mockStory = mockData.stories[0];
+    const expected = {
+      type: 'ADD_STORY',
+      story: mockStory
+    };
+    const result = actions.addStory(mockStory);
+    expect(result).toEqual(expected);
+  });
+
+  it('should return an object with ADD_PHOTO and a photo', () => {
+    const mockPhoto = mockData.photos[0];
+    const expected = {
+      type: 'ADD_PHOTO',
+      photo: mockPhoto
+    };
+    const result = actions.addPhoto(mockPhoto);
+    expect(result).toEqual(expected);
+  });
+
+  it('should retun an object with DELETE_FAMILY and an id', () => {
+    const expected = {
+      type: 'DELETE_FAMILY',
+      id: 2
+    };
+    const result = actions.deleteFamily(2);
+    expect(result).toEqual(expected);
+  });
+
+  it('should retun an object with DELETE_RECIPE and an id', () => {
+    const expected = {
+      type: 'DELETE_RECIPE',
+      id: 3
+    };
+    const result = actions.deleteRecipe(3);
+    expect(result).toEqual(expected);
+  });
+
+  it('should retun an object with DELETE_STORY and an id', () => {
+    const expected = {
+      type: 'DELETE_STORY',
+      id: 4
+    };
+    const result = actions.deleteStory(4);
+    expect(result).toEqual(expected);
+  });
+
+  it('should retun an object with DELETE_PHOTO and an id', () => {
+    const expected = {
+      type: 'DELETE_PHOTO',
+      id: 5
+    };
+    const result = actions.deletePhoto(5);
+    expect(result).toEqual(expected);
+  });
+
+  it('should return an object with UPDATE_FAMILY and a family', () => {
+    const mockFamily = mockData.families[0];
+    const expected = {
+      type: 'UPDATE_FAMILY',
+      family: mockFamily
+    };
+    const result = actions.updateFamily(mockFamily);
+    expect(result).toEqual(expected);
+  });
+
+  it('should return an object with UPDATE_RECIPE and a Recipe', () => {
+    const mockRecipe = mockData.recipes[0];
+    const expected = {
+      type: 'UPDATE_RECIPE',
+      recipe: mockRecipe
+    };
+    const result = actions.updateRecipe(mockRecipe);
+    expect(result).toEqual(expected);
+  });
+
+  it('should return an object with UPDATE_STORY and a story', () => {
+    const mockStory = mockData.stories[0];
+    const expected = {
+      type: 'UPDATE_STORY',
+      story: mockStory
+    };
+    const result = actions.updateStory(mockStory);
+    expect(result).toEqual(expected);
+  });
+
+  it('should return an object with UPDATE_PHOTO and a photo', () => {
+    const mockPhoto = mockData.photos[0];
+    const expected = {
+      type: 'UPDATE_PHOTO',
+      photo: mockPhoto
+    };
+    const result = actions.updatePhoto(mockPhoto);
+    expect(result).toEqual(expected);
+  });
 });
