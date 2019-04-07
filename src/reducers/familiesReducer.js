ea002025abdc7d44747386c9d1ae054a71ee124b
Dylan Hofmann <dylan.hofmann@gmail.com>
2019-04-07 11:51:36 -0600

    Add ADD_FAMILY, DELETE_FAMILY and UPDATE_FAMILY types to families Reducer and tests for those types

--- src/reducers/familiesReducer.js
+++ src/reducers/familiesReducer.js
@@ -2,6 +2,22 @@ export const familiesReducer = (state = [], action) => {
   switch(action.type) {
     case 'SET_FAMILIES':
       return action.families;
+    case 'ADD_FAMILY':
+      return [...state, action.family]
+    case 'DELETE_FAMILY':
+      const filteredFamilies = state.filter(family=> {
+        return family.id !== action.id
+      })
+      return filteredFamilies
+    case 'UPDATE_FAMILY':
+      const updatedFamilies = state.map(family => {
+        if(family.id === action.family.id) {
+          return action.family
+        } else {
+          return family
+        }
+      })
+      return updatedFamilies
     default: 
       return state;
   }
