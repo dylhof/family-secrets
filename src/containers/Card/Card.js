2b9ef2b56a4a5063ec195d32192aa7a0401798dd
Dylan Hofmann <dylan.hofmann@gmail.com>
2019-04-08 18:18:42 -0600

    Add edit icon to cards and connect to forms via props

--- src/containers/Card/Card.js
+++ src/containers/Card/Card.js
@@ -4,19 +4,22 @@ import PropTypes from 'prop-types';
 
 export class Card extends Component {
   generateCard = () => {
-    const { currentView, title, story, name, ingredients, instructions, photo, caption} = this.props
+    const { currentView, title, story, name, ingredients, instructions, photo, caption, showForm} = this.props
     switch(currentView) {
       case 'stories':
         return(
           <div className='Card--div'>
             <h3>{title}</h3>
+            <i className="fas fa-pencil-alt" onClick={() => showForm({title, story})}></i>
             <p className='Card--p--story'>{story}</p>
           </div>
         ) 
       case 'recipes':
+        const ingredientArray = ingredients.split('\n');
         return(
           <div className='Card--div'>
             <h3>{name}</h3>
+            <i className="fas fa-pencil-alt" onClick={() => showForm({name, ingredients: ingredientArray, instructions})}></i>
             <p>ingredients</p>
             <div>{this.generateIngredients(ingredients)}</div>
             <p>instructions</p>
@@ -28,6 +31,7 @@ export class Card extends Component {
           <div className='Card--div'>
             <img src={photo} alt={caption}/>
             <p>{caption}</p>
+            <i className="fas fa-pencil-alt" onClick={() => showForm({caption})}></i>
           </div>
         )
       default: 
