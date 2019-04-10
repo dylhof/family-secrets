fd3d48e74fd3fe0f3509f095001d656dafcd9eba
Dylan Hofmann <dylan.hofmann@gmail.com>
2019-04-10 15:18:41 -0600

    Add CLEAR_ERROR type to error reducer

--- src/reducers/errorReducer.js
+++ src/reducers/errorReducer.js
@@ -2,6 +2,8 @@ export const errorReducer = (state = '', action) => {
   switch(action.type) {
     case 'SET_ERROR':
       return action.error
+    case 'CLEAR_ERROR':
+      return ''
     default:
       return state
   };
