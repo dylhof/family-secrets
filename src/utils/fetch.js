2c43217c131649f0ee5472dbf4324fbc032192fa
Dylan Hofmann <dylan.hofmann@gmail.com>
2019-04-09 18:04:27 -0600

    Add check for status 204 in fetch util

--- src/utils/fetch.js
+++ src/utils/fetch.js
@@ -1,19 +1,19 @@
 import { cleanData } from './cleanData';
 
-export const fetchData = async (path, options) => {
+export const fetchData = async (path, options, id) => {
   const response = await fetch(`${process.env.REACT_APP_BACKEND_URL}api/v1/${path}`, options)
   if (response.status >= 300) {
     const error = await response.json()
     throw new Error(error)
-  } else { 
+  } else if(response.status === 204){ 
+    return id
+  } else {
     let data = await response.json()
     if(data.data) {
       data = cleanData(data)
-      // console.log('from fetchData: ', data)
       return data
     }
-      return data
-    
+      return data 
   }
 };
 
