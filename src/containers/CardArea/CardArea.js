dc749a35abc7597f3c15be7dea9803a40ec1c6b4
Dylan Hofmann <dylan.hofmann@gmail.com>
2019-04-09 18:12:19 -0600

    Fix error

--- src/containers/CardArea/CardArea.js
+++ src/containers/CardArea/CardArea.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-// import * as mockData from '../../mockData';
+import * as mockData from '../../mockData';
 import Card from '../Card/Card';
 import StoryForm from '../StoryForm/StoryForm';
 import RecipeForm from '../RecipeForm/RecipeForm';
